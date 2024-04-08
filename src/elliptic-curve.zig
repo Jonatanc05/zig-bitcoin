6e4d4c06d590ea9f9f0288a0e20c66668df05e9b
Jonata Novais Cirqueira <jonatanc0511@gmail.com>
2024-04-08 16:57:07 -0300

    feat: sum of elliptic curves

---
+++ src/elliptic-curve.zig
@@ -0,0 +1,88 @@
+const std = @import("std");
+const inf = std.math.inf(f64);
+
+pub const CurvePoint = struct {
+    x: f64,
+    y: f64,
+    a: f64,
+    b: f64,
+
+    pub fn init(x: f64, y: f64, a: f64, b: f64) CurvePoint {
+        if (x == inf or y == inf) {
+            std.debug.assert(x == y);
+            return CurvePoint{
+                .x = x,
+                .y = y,
+                .a = a,
+                .b = b,
+            };
+        }
+
+        std.debug.assert(y * y == x * x * x + a * x + b);
+        return CurvePoint{
+            .x = x,
+            .y = y,
+            .a = a,
+            .b = b,
+        };
+    }
+
+    pub fn atInfinity(self: CurvePoint) bool {
+        const atInf: bool = (self.x == inf or self.y == inf);
+        if (atInf) std.debug.assert(self.x == self.y);
+        return atInf;
+    }
+
+    pub fn eq(self: CurvePoint, other: CurvePoint) bool {
+        std.debug.assert(self.a == other.a or self.b == other.b);
+        return self.x == other.x and self.y == other.y;
+    }
+
+    pub fn add(self: CurvePoint, other: CurvePoint) CurvePoint {
+        std.debug.assert(self.a == other.a and self.b == other.b);
+
+        // One of the points is at infinity
+        if (self.atInfinity()) return other;
+        if (other.atInfinity()) return self;
+
+        // Vertical line
+        if (self.x == other.x) {
+            if (self.y == -other.y) {
+                return CurvePoint.init(inf, inf, self.a, self.b);
+            } else if (self.y == other.y) {
+                unreachable;
+            } else unreachable; // ?
+        }
+
+        // Standard case
+        const s = (other.y - self.y) / (other.x - self.x);
+        const x = s * s - self.x - other.x;
+        const y = s * (x - self.x) + self.y;
+        return CurvePoint.init(x, y, self.a, self.b);
+    }
+};
+
+// --------------------- TESTS -------------------------------------
+
+const expect = @import("std").testing.expect;
+
+test "point at infinity" {
+    const I = CurvePoint.init(inf, inf, 5, 7);
+    try expect(I.x == inf);
+    try expect(I.y == inf);
+}
+
+test "point at inf sum (invertibility)" {
+    const p1 = CurvePoint.init(-1, -1, 5, 7);
+    const p2 = CurvePoint.init(-1, 1, 5, 7);
+    const I = CurvePoint.init(inf, inf, 5, 7);
+    try expect(p1.add(I).eq(p1));
+    try expect(I.add(p2).eq(CurvePoint.init(-1, 1, 5, 7)));
+    try expect(p1.add(p2).eq(CurvePoint.init(inf, inf, 5, 7)));
+}
+
+test "sum" {
+    const a = CurvePoint.init(2, 5, 5, 7);
+    const b = CurvePoint.init(-1, -1, 5, 7);
+    try expect(a.add(b).eq(CurvePoint.init(3, 7, 5, 7)));
+}
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const FieldElement = @import("finite-field.zig").FieldElement;
+const CurvePoint = @import("elliptic-curve.zig").CurvePoint;
 
 pub fn main() !void {
-    std.debug.print("------------- FiniteFields -------------\n", .{});
+    std.debug.print("\n------------- FiniteFields -------------\n", .{});
     {
         const a = FieldElement.init(10, 13);
         const b = FieldElement.init(5, 13);
@@ -15,5 +16,20 @@ pub fn main() !void {
         std.debug.print("a ** 2: {}\n", .{a.pow(2)});
         std.debug.print("a / b: {}\n", .{a.div(b)});
         std.debug.print("\n", .{});
+    }
+
+    std.debug.print("------------- EllipticCurves -------------\n", .{});
+    {
+        const p1 = CurvePoint.init(2, 5, 5, 7);
+        const p2 = CurvePoint.init(-1, -1, 5, 7);
+        std.debug.print("Point p1: {}\n", .{p1});
+        std.debug.print("Point p2: {}\n", .{p2});
+        std.debug.print("p1 + p2: {}\n", .{p1.add(p2)});
+        //std.debug.print("p1 - p2: {}\n", .{p1.sub(p2)});
+        //std.debug.print("p2 - p1: {}\n", .{p2.sub(p1)});
+        //std.debug.print("p1 * p2: {}\n", .{p1.mul(p2)});
+        //std.debug.print("p1 ** 2: {}\n", .{p1.pow(2)});
+        //std.debug.print("p1 / p2: {}\n", .{p1.div(p2)});
+        //std.debug.print("\n", .{});
     }
 }

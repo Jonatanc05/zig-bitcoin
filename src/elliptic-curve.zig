38efa5a91d949f4618914eca39ca6c51eff86b57
Jonata Novais Cirqueira <jonatanc0511@gmail.com>
2024-04-08 18:18:01 -0300

    feat: sum equal points

--- src/elliptic-curve.zig
+++ src/elliptic-curve.zig
@@ -47,20 +47,21 @@ pub const CurvePoint = struct {
     pub fn add(self: CurvePoint, other: CurvePoint) CurvePoint {
         std.debug.assert(self.a == other.a and self.b == other.b);
 
-        // One of the points is at infinity
         if (self.atInfinity()) return other;
         if (other.atInfinity()) return self;
 
-        // Vertical line
         if (self.x == other.x) {
-            if (self.y == -other.y) {
+            if (self.y == other.y) {
+                if (self.y == 0) return CurvePoint.init(inf, inf, self.a, self.b);
+                const s = (3 * self.x * self.x + self.a) / (2 * self.y);
+                const x = s * s - self.x - other.x;
+                const y = s * (x - self.x) + self.y;
+                return CurvePoint.init(x, y, self.a, self.b);
+            } else if (self.y == -other.y) {
                 return CurvePoint.init(inf, inf, self.a, self.b);
-            } else if (self.y == other.y) {
-                unreachable;
-            } else unreachable; // ?
+            } else unreachable;
         }
 
-        // Standard case
         const s = (other.y - self.y) / (other.x - self.x);
         const x = s * s - self.x - other.x;
         const y = s * (x - self.x) + self.y;
@@ -91,4 +92,16 @@ test "sum" {
     const a = CurvePoint.init(2, 5, 5, 7);
     const b = CurvePoint.init(-1, -1, 5, 7);
     try expect(a.add(b).eq(CurvePoint.init(3, 7, 5, 7)));
+}
+
+test "sum equal points" {
+    const a = CurvePoint.init(-1, -1, 5, 7);
+    const b = CurvePoint.init(-1, -1, 5, 7);
+    try expect(a.add(b).eq(CurvePoint.init(18, -77, 5, 7)));
+}
+
+test "sum equal points at y=0" {
+    const a = CurvePoint.init(0, 0, 5, 0);
+    const b = CurvePoint.init(0, 0, 5, 0);
+    try expect(a.add(b).eq(CurvePoint.init(inf, inf, 5, 7)));
 }

5b8155eb65fc9e789e753acda010a39c90e50e15
Jonata Novais Cirqueira <jonatanc0511@gmail.com>
2024-05-01 13:52:09 -0300

    refactor: generateKeyPair()

--- src/crypt.zig
+++ src/crypt.zig
@@ -5,11 +5,11 @@ const NumberType = FieldElementLib.NumberType;
 const HalfNumberType = FieldElementLib.HalfNumberType;
 const fe = FieldElementLib.fieldElementShortcut;
 const CurvePoint = @import("elliptic-curve.zig").CurvePoint;
-
 const secp256k1_a = 0;
 const secp256k1_b = 7;
 const secp256k1_Gx = 0x79be667e_f9dcbbac_55a06295_ce870b07_029bfcdb_2dce28d9_59f2815b_16f81798;
 const secp256k1_Gy = 0x483ada77_26a3c465_5da4fbfc_0e1108a8_fd17b448_a6855419_9c47d08f_fb10d4b8;
+
 pub const G = CurvePoint.init(
     FieldElement.init(secp256k1_Gx, secp256k1_p),
     FieldElement.init(secp256k1_Gy, secp256k1_p),
@@ -25,10 +25,15 @@ pub const Signature = struct {
     s: NumberType,
 };
 
+pub fn generateKeyPair() struct { P: CurvePoint, e: NumberType } {
+    const e = randomizer.random().int(HalfNumberType);
+    const P = G.muli(e);
+    return .{ .P = P, .e = e };
+}
+
 /// CRYPTOGRAPHICALLY UNSAFE. @TODO: use RFC 6979
 pub fn sign(z: NumberType, e: NumberType) Signature {
-    var rand = std.Random.Xoshiro256.init(0);
-    const k = rand.random().int(HalfNumberType);
+    const k = randomizer.random().int(HalfNumberType);
     const r = G.muli(k).x.?.value;
     const k_inv = modpow(k, secp256k1_n - 2, secp256k1_n);
     const s = @mod(@mod((z + @mod(r * e, secp256k1_n)), secp256k1_n) * k_inv, secp256k1_n);
@@ -41,6 +46,8 @@ pub fn verify(z: NumberType, P: CurvePoint, sig: Signature) bool {
     const v = @mod(sig.r * s_inv, secp256k1_n);
     return G.muli(u).add(P.muli(v)).x.?.value == sig.r;
 }
+
+var randomizer = std.Random.Xoshiro256.init(0);
 
 fn modpow(base: NumberType, exponent: NumberType, modulo: NumberType) NumberType {
     var base_mod = @mod(base, modulo);
@@ -71,8 +78,9 @@ test "signing message" {
     // Mock hash of a hypothetical message
     const z = 0x231c6f3d980a6b0fb7152f85cee7eb52bf92433d9919b9c5218cb08e79cce78;
     // Signing (hypothetical private key)
-    const e = 0x8b387de39861728c92ec9f589c303b1038ff60eb3963b12cd212263a1d1e0f00;
-    const P = G.muli(e);
+    const keys = generateKeyPair();
+    const e = keys.e;
+    const P = keys.P;
 
     const signature: Signature = sign(z, e);
     const valid = verify(z, P, signature);

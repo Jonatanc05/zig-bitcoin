71f9dcc1b246a36a0891074bebafad6fc57c34b4
Jonata Novais Cirqueira <jonatanc0511@gmail.com>
2024-05-06 17:58:55 -0300

    feat: SEC serialization

--- src/crypt.zig
+++ src/crypt.zig
@@ -5,18 +5,26 @@ const NumberType = FieldElementLib.NumberType;
 const MulExtendedNumberType = FieldElementLib.MulExtendedNumberType;
 const fe = FieldElementLib.fieldElementShortcut;
 const CurvePoint = @import("elliptic-curve.zig").CurvePoint;
+const assert = std.debug.assert;
 const native_endian = @import("builtin").target.cpu.arch.endian();
+
+comptime {
+    if (NumberType != u256) {
+        // This is because we use secp256k1
+        @compileError("Only NumberType = u256 is supported");
+    }
+}
 
 const secp256k1_a = 0;
 const secp256k1_b = 7;
-const secp256k1_Gx = 0x79be667e_f9dcbbac_55a06295_ce870b07_029bfcdb_2dce28d9_59f2815b_16f81798;
-const secp256k1_Gy = 0x483ada77_26a3c465_5da4fbfc_0e1108a8_fd17b448_a6855419_9c47d08f_fb10d4b8;
+const secp256k1_a_fe = FieldElement.init(secp256k1_a, secp256k1_p);
+const secp256k1_b_fe = FieldElement.init(secp256k1_b, secp256k1_p);
 
 pub const G = CurvePoint.init(
-    FieldElement.init(secp256k1_Gx, secp256k1_p),
-    FieldElement.init(secp256k1_Gy, secp256k1_p),
-    FieldElement.init(secp256k1_a, secp256k1_p),
-    FieldElement.init(secp256k1_b, secp256k1_p),
+    FieldElement.init(0x79be667e_f9dcbbac_55a06295_ce870b07_029bfcdb_2dce28d9_59f2815b_16f81798, secp256k1_p),
+    FieldElement.init(0x483ada77_26a3c465_5da4fbfc_0e1108a8_fd17b448_a6855419_9c47d08f_fb10d4b8, secp256k1_p),
+    secp256k1_a_fe,
+    secp256k1_b_fe,
 );
 
 pub const secp256k1_p = 0xffffffff_ffffffff_ffffffff_ffffffff_ffffffff_ffffffff_fffffffe_fffffc2f;
@@ -28,7 +36,7 @@ pub const Signature = struct {
 };
 
 pub fn hash(message: []const u8) NumberType {
-    const bytesInNumberType: comptime_int = @divExact(@typeInfo(NumberType).Int.bits, 8);
+    const bytesInNumberType = @divExact(@typeInfo(NumberType).Int.bits, 8);
     var z_bytes: [bytesInNumberType]u8 = undefined;
     std.crypto.hash.sha2.Sha256.hash(message, z_bytes[0..bytesInNumberType], .{});
     return std.mem.readInt(NumberType, &z_bytes, native_endian);
@@ -77,6 +85,65 @@ pub fn verify(z: NumberType, P: CurvePoint, sig: Signature) bool {
     return G.muli(u).add(P.muli(v)).x.?.value == sig.r;
 }
 
+fn bytesNeeded(comptime T: type, compressed: bool) comptime_int {
+    const bytes_in_type = @divExact(@typeInfo(T).Int.bits, 8);
+    if (compressed) {
+        return bytes_in_type + 1;
+    } else {
+        return 2 * bytes_in_type + 1;
+    }
+}
+pub fn serialize(point: CurvePoint, comptime compressed: bool, out: *[bytesNeeded(NumberType, compressed)]u8) void {
+    assert(point.x != null and point.y != null); // @TODO infinity
+    assert(point.x.?.prime == secp256k1_p);
+    const bytes_in_number_type = @divExact(@typeInfo(FieldElementLib.NumberType).Int.bits, 8);
+    var x_bytes: [bytes_in_number_type]u8 = undefined;
+    std.mem.writeInt(NumberType, &x_bytes, point.x.?.value, std.builtin.Endian.big);
+
+    if (compressed) {
+        assert(out.len >= bytes_in_number_type + 1);
+        if (point.y.?.value % 2 == 0) {
+            out.* = [1]u8{0x02} ++ x_bytes;
+        } else {
+            out.* = [1]u8{0x03} ++ x_bytes;
+        }
+    } else {
+        assert(out.len >= 2 * bytes_in_number_type + 1);
+        var y_bytes: [bytes_in_number_type]u8 = undefined;
+        std.mem.writeInt(NumberType, &y_bytes, point.y.?.value, std.builtin.Endian.big);
+        out.* = [1]u8{0x04} ++ x_bytes ++ y_bytes;
+    }
+}
+
+pub fn parse(bytes: []const u8) CurvePoint {
+    assert(bytes.len > 0);
+    switch (bytes[0]) {
+        0x02, 0x03 => {
+            assert(bytes.len == 33);
+            const x = FieldElement.init(
+                std.mem.readInt(NumberType, bytes[1..33], std.builtin.Endian.big),
+                secp256k1_p,
+            );
+            const y_squared = x.pow(3).add(secp256k1_b_fe);
+            const y = modpow(y_squared.value, @divFloor(secp256k1_p + 1, 4), secp256k1_p);
+            const even_y = if (y % 2 == 0) FieldElement.init(y, secp256k1_p) else FieldElement.init(secp256k1_p - y, secp256k1_p);
+            const odd_y = if (y % 2 == 1) FieldElement.init(y, secp256k1_p) else FieldElement.init(secp256k1_p - y, secp256k1_p);
+            if (bytes[0] == 0x02) {
+                return CurvePoint.init(x, even_y, secp256k1_a_fe, secp256k1_b_fe);
+            } else if (bytes[0] == 0x03) {
+                return CurvePoint.init(x, odd_y, secp256k1_a_fe, secp256k1_b_fe);
+            } else unreachable;
+        },
+        0x04 => {
+            assert(bytes.len == 65);
+            const x = FieldElement.init(std.mem.readInt(NumberType, bytes[1..33], std.builtin.Endian.big), secp256k1_p);
+            const y = FieldElement.init(std.mem.readInt(NumberType, bytes[33..65], std.builtin.Endian.big), secp256k1_p);
+            return CurvePoint.init(x, y, secp256k1_a_fe, secp256k1_b_fe);
+        },
+        else => unreachable,
+    }
+}
+
 fn modpow(base: NumberType, exponent: NumberType, modulo: NumberType) NumberType {
     var base_mod = @mod(base, modulo);
     var result: MulExtendedNumberType = 1;
@@ -99,11 +166,6 @@ fn modpow(base: NumberType, exponent: NumberType, modulo: NumberType) NumberType
 
 const expect = std.testing.expect;
 
-test "G is on curve" {
-    FieldElementLib.setGlobalPrime(secp256k1_p);
-    _ = CurvePoint.init(fe(secp256k1_Gx), fe(secp256k1_Gy), fe(secp256k1_a), fe(secp256k1_b));
-}
-
 test "order of G is indeed n" {
     try expect(G.muli(secp256k1_n).atInfinity());
 }
@@ -114,4 +176,18 @@ test "signing message" {
     const signature: Signature = sign(z, keys.prvk);
     const valid = verify(z, keys.pubk, signature);
     try expect(valid);
+}
+
+test "sec serialization and parsing" {
+    const p1 = G.muli(3858);
+
+    var p1_uncompressed: [1 + 2 * @divExact(@typeInfo(NumberType).Int.bits, 8)]u8 = undefined;
+    serialize(p1, false, &p1_uncompressed);
+    const p1_uncompressed_parsed = parse(p1_uncompressed[0..]);
+    try expect(p1_uncompressed_parsed.eq(p1));
+
+    var p1_compressed: [1 + @divExact(@typeInfo(NumberType).Int.bits, 8)]u8 = undefined;
+    serialize(p1, true, &p1_compressed);
+    const p1_compressed_parsed = parse(p1_compressed[0..]);
+    try expect(p1_compressed_parsed.eq(p1));
 }
--- src/main.zig
+++ src/main.zig
@@ -5,61 +5,91 @@ const NumberType = FieldElementLib.NumberType;
 const fe = FieldElementLib.fieldElementShortcut;
 const CurvePoint = @import("elliptic-curve.zig").CurvePoint;
 const CryptLib = @import("crypt.zig");
+const print = std.debug.print;
 
 pub fn main() !void {
-    std.debug.print("\n------------- FiniteFields -------------\n", .{});
+    print("\n------------- FiniteFields -------------\n", .{});
     {
         FieldElementLib.setGlobalPrime(13);
         const a = fe(10);
         const b = fe(5);
-        std.debug.print("Element a: {}\n", .{a});
-        std.debug.print("Element b: {}\n", .{b});
-        std.debug.print("a + b: {}\n", .{a.add(b)});
-        std.debug.print("a - b: {}\n", .{a.sub(b)});
-        std.debug.print("b - a: {}\n", .{b.sub(a)});
-        std.debug.print("a * b: {}\n", .{a.mul(b)});
-        std.debug.print("a ** 2: {}\n", .{a.pow(2)});
-        std.debug.print("a ** 3: {}\n", .{a.pow(3)});
-        std.debug.print("a / b: {}\n", .{a.div(b)});
-        std.debug.print("\n", .{});
+        print("Element a: {}\n", .{a});
+        print("Element b: {}\n", .{b});
+        print("a + b: {}\n", .{a.add(b)});
+        print("a - b: {}\n", .{a.sub(b)});
+        print("b - a: {}\n", .{b.sub(a)});
+        print("a * b: {}\n", .{a.mul(b)});
+        print("a ** 2: {}\n", .{a.pow(2)});
+        print("a ** 3: {}\n", .{a.pow(3)});
+        print("a / b: {}\n", .{a.div(b)});
+        print("\n", .{});
     }
 
-    std.debug.print("------------- EllipticCurves -------------\n", .{});
+    print("------------- EllipticCurves -------------\n", .{});
     {
         FieldElementLib.setGlobalPrime(223);
         const a = fe(0);
         const b = fe(7);
         const p1 = CurvePoint.init(fe(192), fe(105), a, b);
         const p2 = CurvePoint.init(fe(17), fe(56), a, b);
-        std.debug.print("Point p1: {}\n", .{p1});
-        std.debug.print("Point p2: {}\n", .{p2});
-        std.debug.print("p1 + p2: {}\n", .{p1.add(p2)});
+        print("Point p1: {}\n", .{p1});
+        print("Point p2: {}\n", .{p2});
+        print("p1 + p2: {}\n", .{p1.add(p2)});
         const p3 = CurvePoint.init(fe(47), fe(71), a, b);
-        std.debug.print("Point p3: {}\n", .{p3});
-        std.debug.print("p3 + p3: {}\n", .{p3.add(p3)});
-        std.debug.print("2 p3: {}\n", .{p3.muli(2)});
-        std.debug.print("3 p3: {}\n", .{p3.muli(3)});
-        std.debug.print("18 p3: {}\n", .{p3.muli(18)});
-        std.debug.print("19 p3: {}\n", .{p3.muli(19)});
-        std.debug.print("20 p3: {}\n", .{p3.muli(20)});
-        std.debug.print("21 p3: {}\n", .{p3.muli(21)});
+        print("Point p3: {}\n", .{p3});
+        print("p3 + p3: {}\n", .{p3.add(p3)});
+        print("2 p3: {}\n", .{p3.muli(2)});
+        print("3 p3: {}\n", .{p3.muli(3)});
+        print("18 p3: {}\n", .{p3.muli(18)});
+        print("19 p3: {}\n", .{p3.muli(19)});
+        print("20 p3: {}\n", .{p3.muli(20)});
+        print("21 p3: {}\n", .{p3.muli(21)});
         var G = CurvePoint.init(fe(15), fe(86), a, b);
         G.computeOrder();
-        std.debug.print("ordem do grupo gerado por {}: {}\n", .{ G, G.order.? });
-        std.debug.print("\n", .{});
+        print("ordem do grupo gerado por {}: {}\n", .{ G, G.order.? });
+        print("\n", .{});
     }
 
-    std.debug.print("-------------- Cryptography --------------\n", .{});
+    print("-------------- Cryptography --------------\n", .{});
     {
         const message = "The quick brown fox jumps over the lazy dog";
-        std.debug.print("message: \"{s}\"\n", .{message});
+        print("message: \"{s}\"\n", .{message});
         const z = CryptLib.hash(message);
-        std.debug.print("h(message): {x}\n", .{z});
+        print("h(message): {x}\n", .{z});
         const keys = CryptLib.generateKeyPair();
-        std.debug.print("Public key: {}\nPrivate key: {x}\n", .{ keys.pubk, keys.prvk });
-        std.debug.print("Signing the message...\n", .{});
+        print("Public key: {}\nPrivate key: {x}\n", .{ keys.pubk, keys.prvk });
+        print("Signing the message...\n", .{});
         const sig = CryptLib.sign(z, keys.prvk);
-        std.debug.print("Verifying the signature...\n", .{});
-        std.debug.print("valid: {}\n", .{CryptLib.verify(z, keys.pubk, sig)});
+        print("Verifying the signature...\n", .{});
+        print("valid: {}\n\n", .{CryptLib.verify(z, keys.pubk, sig)});
+    }
+
+    print("-------------- Serialization --------------\n", .{});
+    {
+        const p1 = CryptLib.G.muli(3858);
+        print("p1.x: 0x{x:0>64}\n", .{p1.x.?.value});
+        print("p1.y: 0x{x:0>64}\n", .{p1.y.?.value});
+
+        var p1_uncompressed: [1 + 2 * @divExact(@typeInfo(NumberType).Int.bits, 8)]u8 = undefined;
+        CryptLib.serialize(p1, false, &p1_uncompressed);
+        print("serialized(p1): 0x", .{});
+        for (p1_uncompressed) |b| {
+            print("{x:0>2}", .{b});
+        }
+        print("\n", .{});
+
+        const p1_uncompressed_parsed = CryptLib.parse(p1_uncompressed[0..]);
+        print("parsed(serialized(p1)) == p1: {}\n", .{p1_uncompressed_parsed.eq(p1)});
+
+        var p1_compressed: [1 + @divExact(@typeInfo(NumberType).Int.bits, 8)]u8 = undefined;
+        CryptLib.serialize(p1, true, &p1_compressed);
+        print("compressed(p1): 0x", .{});
+        for (p1_compressed) |b| {
+            print("{x:0>2}", .{b});
+        }
+        print("\n", .{});
+
+        const p1_compressed_parsed = CryptLib.parse(p1_compressed[0..]);
+        print("parsed(compressed(p1)) == p1: {}\n", .{p1_compressed_parsed.eq(p1)});
     }
 }
